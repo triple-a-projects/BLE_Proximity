1113e1e272999f13dc3ed6edb8dd8baa7069946c
Aditi Kharel <077bei008.aditi@pcampus.edu.np>
2024-03-11 10:41:26 +0545

    editable attendance

--- lib/perspectives/teacher/attendance.dart
+++ lib/perspectives/teacher/attendance.dart
@@ -6,28 +6,19 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:ble_advertiser/animation.dart';
 
-class AttendanceTable extends StatelessWidget {
+class AttendanceTable extends StatefulWidget {
   const AttendanceTable({super.key});
 
+  @override
+  State<AttendanceTable> createState() => _AttendanceTableState();
+}
+
+class _AttendanceTableState extends State<AttendanceTable> {
   @override
   Widget build(BuildContext context) {
     final databaseReference =
         FirebaseDatabase.instance.ref().child('BEI');
     final firestore = FirebaseFirestore.instance;
-
-    databaseReference.onValue.listen((event) {
-      var dataSnapshot = event.snapshot;
-      Map<dynamic, dynamic>? data =
-          dataSnapshot.value as Map<dynamic, dynamic>?;
-      if (data != null) {
-        data.forEach((key, value) async {
-          await firestore.collection('users').doc(key).update({
-            'present': value['Present'],
-          });
-          print('Firestore document updated for rollNo: $key');
-        });
-      }
-    });
 
     return Scaffold(
       appBar: AppBar(
@@ -37,82 +28,126 @@ class AttendanceTable extends StatelessWidget {
         automaticallyImplyLeading: true,
         actions: [
           IconButton(
-              icon: const Icon(
-                Icons.info_outlined,
-                color: lightest,
-                size: 30,
-              ),
-              onPressed: () {
-                Navigator.of(context).push(
-                  PageTransitionAnimation(
-                    page: InfoPage(),
-                  ),
-                );
-              })
+            icon: const Icon(
+              Icons.info_outlined,
+              color: lightest,
+              size: 30,
+            ),
+            onPressed: () {
+              Navigator.of(context).push(
+                PageTransitionAnimation(
+                  page: InfoPage(),
+                ),
+              );
+            },
+          )
         ],
       ),
       body: SingleChildScrollView(
         scrollDirection: Axis.horizontal,
         child: SingleChildScrollView(
           scrollDirection: Axis.vertical,
-        
-        child: FutureBuilder(
-          future: FirebaseFirestore.instance.collection('users').get(),
-          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator(
-                  color: Colors
-                      .white); // Show a loading indicator while fetching data
-            }
-            if (snapshot.hasError) {
-              return Text(
-                  'Error: ${snapshot.error}'); // Show an error message if fetching fails
-            }
-            final users = snapshot.data!.docs;
-            return SingleChildScrollView(
-              child: DataTable(
-                columns: <DataColumn>[
-                  const DataColumn(
-                    label: Text(
-                      'Roll No',
-                      style: TextStyle(fontStyle: FontStyle.italic),
+          child: FutureBuilder(
+            future: FirebaseFirestore.instance.collection('users').get(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator(
+                  color: Colors.white,
+                );
+              }
+              if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              }
+              final users = snapshot.data!.docs;
+              return SingleChildScrollView(
+                child: DataTable(
+                  columns: <DataColumn>[
+                    DataColumn(
+                      label: Text(
+                        'Roll No',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      ),
                     ),
-                  ),
-                  const DataColumn(
-                    label: Text(
-                      'Name',
-                      style: TextStyle(fontStyle: FontStyle.italic),
+                    const DataColumn(
+                      label: Text(
+                        'Name',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      ),
                     ),
-                  ),
-                  DataColumn(
-                    label: Text(
-                      DateFormat('yyyy-MM-dd').format(DateTime.now()),
-                      style: const TextStyle(fontStyle: FontStyle.italic),
+                    DataColumn(
+                      label: Text(
+                        DateFormat('yyyy-MM-dd').format(DateTime.now()),
+                        style: const TextStyle(fontStyle: FontStyle.italic),
+                      ),
                     ),
-                  ),
-                  const DataColumn(
-                    label: Text(
-                      'Phone Number',
-                      style: TextStyle(fontStyle: FontStyle.italic),
+                    const DataColumn(
+                      label: Text(
+                        'Phone Number',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      ),
                     ),
-                  ),
-                ],
-                rows: List<DataRow>.generate(
-                  users.length,
-                  (index) => DataRow(
-                    cells: <DataCell>[
-                      DataCell(Text(users[index]['rollNo'])),
-                      DataCell(Text(users[index]['name'])),
-                      DataCell(Text(users[index]['present']== true ? 'P' : 'A',)),
-                      DataCell(Text(users[index]['phoneNo'])),
-                    ],
+                  ],
+                  rows: List<DataRow>.generate(
+                    users.length,
+                    (index) => DataRow(
+                      cells: <DataCell>[
+                        DataCell(
+                          Text(users[index]['rollNo']),
+                        ),
+                        DataCell(
+                          Text(users[index]['name']),
+                        ),
+                        DataCell(
+                          Row(
+                            children: [
+                              FutureBuilder(
+                                future: FirebaseFirestore.instance.collection('users').doc(users[index].id).get(),
+                                builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                                  if (snapshot.connectionState == ConnectionState.waiting) {
+                                    return CircularProgressIndicator(color: Colors.white);
+                                  }
+                                  if (snapshot.hasError) {
+                                    return Text('Error: ${snapshot.error}');
+                                  }
+                                  var present = snapshot.data!['present'];
+                                  return DropdownButton<String>(
+                                    onChanged: (String? newValue) async {
+                                      if (newValue != null) {
+                                        bool updatedValue = newValue == 'P' ? true : false;
+                                        // Update the Firestore document with the selected value
+                                        await firestore.collection('users').doc(users[index].id).update({
+                                          'present': updatedValue,
+                                        }).then((_){
+                                          setState(() {
+                                            present=updatedValue;
+                                          });
+                                        });
+                                      }
+                                    },
+                                    value: present == true ? 'P' : 'A',
+                                    items: <String>['A', 'P'].map<DropdownMenuItem<String>>((String value) {
+                                      return DropdownMenuItem<String>(
+                                        value: value,
+                                        child: Text(value),
+                                      );
+                                    }).toList(),
+                                  );
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                        DataCell(
+                          Text(users[index]['phoneNo']),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            );
-          },
+              );
+            },
+          ),
         ),
-        )
       ),
     );
   }
