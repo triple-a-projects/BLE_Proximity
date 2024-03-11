9695e2c15565fac36a6e739abaa10ab1d0c162b8
Aditi Kharel <077bei008.aditi@pcampus.edu.np>
2024-03-11 08:48:12 +0545

    attendance table rtdb

--- lib/perspectives/teacher/attendance.dart
+++ lib/perspectives/teacher/attendance.dart
@@ -1,6 +1,7 @@
 import 'package:ble_advertiser/colors.dart';
 import 'package:ble_advertiser/info.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -9,6 +10,24 @@ class AttendanceTable extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final databaseReference =
+        FirebaseDatabase.instance.ref().child('BEI');
+    final firestore = FirebaseFirestore.instance;
+
+    databaseReference.onValue.listen((event) {
+      var dataSnapshot = event.snapshot;
+      Map<dynamic, dynamic>? data =
+          dataSnapshot.value as Map<dynamic, dynamic>?;
+      if (data != null) {
+        data.forEach((key, value) async {
+          await firestore.collection('users').doc(key).update({
+            'present': value['Present'],
+          });
+          print('Firestore document updated for rollNo: $key');
+        });
+      }
+    });
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Attendance'),
@@ -45,43 +64,45 @@ class AttendanceTable extends StatelessWidget {
                   'Error: ${snapshot.error}'); // Show an error message if fetching fails
             }
             final users = snapshot.data!.docs;
-            return DataTable(
-              columns: <DataColumn>[
-                const DataColumn(
-                  label: Text(
-                    'Roll No',
-                    style: TextStyle(fontStyle: FontStyle.italic),
+            return SingleChildScrollView(
+              child: DataTable(
+                columns: <DataColumn>[
+                  const DataColumn(
+                    label: Text(
+                      'Roll No',
+                      style: TextStyle(fontStyle: FontStyle.italic),
+                    ),
                   ),
-                ),
-                const DataColumn(
-                  label: Text(
-                    'Name',
-                    style: TextStyle(fontStyle: FontStyle.italic),
+                //   const DataColumn(
+                //     label: Text(
+                //       'Name',
+                //       style: TextStyle(fontStyle: FontStyle.italic),
+                //     ),
+                //   ),
+                  DataColumn(
+                    label: Text(
+                      DateFormat('yyyy-MM-dd').format(DateTime.now()),
+                      style: const TextStyle(fontStyle: FontStyle.italic),
+                    ),
                   ),
-                ),
-                DataColumn(
-                  label: Text(
-                    DateFormat('yyyy-MM-dd').format(DateTime.now()),
-                    style: const TextStyle(fontStyle: FontStyle.italic),
+                //   const DataColumn(
+                //     label: Text(
+                //       'Phone Number',
+                //       style: TextStyle(fontStyle: FontStyle.italic),
+                //     ),
+                //   ),
+                ],
+                rows: List<DataRow>.generate(
+                  users.length,
+                  (index) => DataRow(
+                    cells: <DataCell>[
+                      DataCell(Text(users[index]['rollNo'])),
+                      // DataCell(Text(users[index]['name'])),
+                      DataCell(Text(users[index]['present']== true ? 'P' : 'A',)),
+                      // DataCell(Text(users[index]['phoneNo'])),
+                      // Placeholder for attendance
+                    ],
                   ),
-                ),
-                const DataColumn(
-                  label: Text(
-                    'Phone Number',
-                    style: TextStyle(fontStyle: FontStyle.italic),
-                  ),
-                ),
-              ],
-              rows: List<DataRow>.generate(
-                users.length,
-                (index) => DataRow(
-                  cells: <DataCell>[
-                    DataCell(Text(users[index]['rollNo'])),
-                    DataCell(Text(users[index]['name'])),
-                    DataCell(Text(users[index]['present'])),
-                    DataCell(Text(users[index]['phoneNo'])),
-                    // Placeholder for attendance
-                  ],
                 ),
               ),
             );
